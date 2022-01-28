ba65bf7dc10f0cb7d9b1a73ff27e9ec970ac5fd9
SerhiiTrytenko <t3coder@gmail.com>
2022-01-28 23:05:57 +0200

    add responsive to section our-mission

--- src/index.js
+++ src/index.js
@@ -20,6 +20,37 @@ if(sliderBlock != null) {
         // loop: true,
         init: false,
         grabCursor: true,
+        breakpoints: {
+            "@0.00": {
+                slidesPerView: "auto",
+                spaceBetween: 20,
+                centeredSlides: true,
+            },
+            "@0.55": {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                centeredSlides: true,
+            },
+            "@0.75": {
+                slidesPerView: 2,
+                spaceBetween: 20,
+                centeredSlides: true,
+            },
+            "@0.85": {
+                slidesPerView: 2,
+                spaceBetween: 20,
+                centeredSlides: true,
+            },
+            "@1.00": {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                centeredSlides: true,
+            },
+            "@1.50": {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+        },
     });
     swiper.init();
 }
