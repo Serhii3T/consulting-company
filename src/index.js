9c95fc7fa97a23ed919bdd521663523e28ce0ab1
SerhiiTrytenko <t3coder@gmail.com>
2022-01-28 20:12:36 +0200

    Create new project test task to beetroot.

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// import of a JavaScript module
+// import { example } from '@/js/example'
+
+// import of styles
+import '@/styles/index.scss'
+
+import Swiper, {Navigation} from "swiper";
+// import styles bundle
+import 'swiper/css/bundle';
+// configure Swiper to use modules
+Swiper.use([
+    Navigation,
+]);
+
+const sliderBlock = document.querySelector(".mySwiper");
+if(sliderBlock != null) {
+    let swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        init: false,
+        grabCursor: true,
+    });
+    swiper.init();
+}
+
---
+++ src/js/swiper.js
@@ -0,0 +1,6 @@
+let swiper = new Swiper(".mySwiper", {
+    direction: "horizontal",
+    spaceBetween: 30,
+    slidesPerView: 3,
+    grabCursor: true,
+});
