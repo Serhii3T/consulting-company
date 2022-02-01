3088ba86e97116794c414da0dc56ac39fac1d77d
SerhiiTrytenko <t3coder@gmail.com>
2022-02-01 23:42:06 +0200

    finish create burger menu and resposive, edited styles all section

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -37,7 +37,7 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
+      title: 'consulting-company',
       favicon: paths.src + '/images/favicon.png',
       template: paths.src + '/index.html', // template file
       filename: 'index.html', // output file
--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-// import of a JavaScript module
-// import { example } from '@/js/example'
-
 // import of styles
 import '@/styles/index.scss'
 
+
+
+// burger menu
+const burger = document.querySelector("#burger");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+const body = document.body;
+
+burger.addEventListener("click", burgerHandler);
+
+function burgerHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    burger.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
+
+
+
+// import swiper
 import Swiper, {Navigation} from "swiper";
 // import styles bundle
 import 'swiper/css/bundle';
@@ -17,34 +39,28 @@ if(sliderBlock != null) {
     let swiper = new Swiper(".mySwiper", {
         slidesPerView: 3,
         spaceBetween: 30,
-        // loop: true,
         init: false,
         grabCursor: true,
         breakpoints: {
             "@0.00": {
                 slidesPerView: "auto",
                 spaceBetween: 20,
-                centeredSlides: true,
             },
             "@0.55": {
                 slidesPerView: 1,
                 spaceBetween: 20,
-                centeredSlides: true,
             },
             "@0.75": {
                 slidesPerView: 2,
                 spaceBetween: 20,
-                centeredSlides: true,
             },
             "@0.85": {
                 slidesPerView: 2,
                 spaceBetween: 20,
-                centeredSlides: true,
             },
             "@1.00": {
                 slidesPerView: 3,
                 spaceBetween: 30,
-                centeredSlides: true,
             },
             "@1.50": {
                 slidesPerView: 3,
@@ -54,4 +70,6 @@ if(sliderBlock != null) {
     });
     swiper.init();
 }
+
+
 
